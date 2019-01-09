e39e1f3e6ea87cb4493223a8079b59cc34457a82
beatricecourage <bea_courage@hotmail.co.uk>
2019-01-09 09:26:10 +0000

    Implemented sessions to display message with particular ID in a seperate page

--- app.rb
+++ app.rb
@@ -22,10 +22,14 @@ set :sessions, true
     redirect ("/")
   end
 
-  get "/full_message/:id" do
-    messages = session[:messages]
-    @message = messages[Integer(params[:id])]
-    erb(:all_messages)
+  get "/messages/:id" do
+    session[:messages].each do |message|
+    #return the message that has the particular id which was stored in the param
+      @message = message if message.id == params[:id].to_i
+    end
+    erb :message
   end
-
 end
+
+# i want to find a message with a particular id
+# corresponds to the id of the message that was clicked and that is found in the param
--- lib/message.rb
+++ lib/message.rb
@@ -5,21 +5,28 @@ require 'data_mapper'
   #   connection = PG.connect(dbname: 'messanger_db')
   #   connection.exec('SELECT * FROM messages;')
   # end
-  
-class Message
-  include Data_Mapper:: Resource
-  attr_reader :content, :time
 
-property :id, Serial
-property :message, String
+
+class Message
+  @@id = 0
+#   include DataMapper:: Resource
+  attr_reader :content, :time, :id
+#
+# property :id, Serial
+# property :text, String
+# property :created_at, DateTime
+
 
   def initialize(content)
     @content = content
     @time = Time.now
+    @@id += 1
+    @id = @@id
   end
 
   def twenty_characters
     @content[0...20]
   end
+
 
 end
